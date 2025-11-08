cda7d20808f290cff3732ef4f69e5675ea8e7f67
primev1bots <niloymina139121@gmail.com>
2025-11-08 22:57:48 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1522,7 +1522,7 @@ const HomeTab: React.FC = () => {
     const script = document.createElement('script')
     script.src = '//libtl.com/sdk.js'
     script.async = true
-    script.setAttribute('data-zone', '10161260)
+    script.setAttribute('data-zone', '10161260')
     script.setAttribute('data-sdk', 'show_10161260')
     
     script.onload = () => {
